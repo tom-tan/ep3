ff4a2a923c79adbd5d3157bec7f3fff80f3898e8
Tomoya Tanjo <ttanjo@gmail.com>
2020-11-11 11:07:16 +0000

    Remove `status` and `terminate` from subcommands

--- lib/ep3/ep3.rb
+++ lib/ep3/ep3.rb
@@ -2,9 +2,7 @@
 require 'optparse'
 require_relative 'ep3-init'
 require_relative 'ep3-run'
-require_relative 'ep3-status'
 require_relative 'ep3-list'
-require_relative 'ep3-terminate'
 
 if $0 == __FILE__
   parser = OptionParser.new
@@ -31,11 +29,9 @@ EOS
   case cmd
   when 'init'      then ep3_init(ARGV)
   when 'run'       then ep3_run(ARGV)
-  when 'status'    then ep3_status(ARGV)
   when 'list'      then ep3_list(ARGV)
   when 'stop'      then ep3_stop(ARGV)
   when 'resume'    then ep3_resume(ARGV)
-  when 'terminate' then ep3_terminate(ARGV)
   else raise "No such subcommand: #{cmd}"
   end
 end
