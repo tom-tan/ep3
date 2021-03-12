e99c46869cf33234964b9d8a4ac4542562b7ecba
Tomoya Tanjo <ttanjo@gmail.com>
2021-03-12 07:51:27 +0000

    Use non-root user for dev container

---
+++ .devcontainer/postCreate.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo ./.devcontainer/install-medal.sh || exit 1
+sudo ./.devcontainer/install-medal-hook.sh || exit 1
+
+test -d cwl || git clone --depth 1 https://github.com/common-workflow-language/common-workflow-language.git /workspace/ep3/cwl
