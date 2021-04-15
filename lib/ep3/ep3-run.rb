be051358b6edfcbc58fee8c55ee501df1f5ddaf7
Tomoya Tanjo <ttanjo@gmail.com>
2021-04-15 10:25:31 +0000

    Cosmetic change

--- lib/ep3/ep3-run.rb
+++ lib/ep3/ep3-run.rb
@@ -55,58 +55,55 @@ def ep3_run(args)
     raise "Directory not found: #{dir}"
   end
 
-  medal_pid = nil
-  begin
-    open(File.join(dir, 'workdir', 'init.yml'), 'w') { |f|
-      f.puts <<EOS
+  open(File.join(dir, 'workdir', 'init.yml'), 'w') { |f|
+    f.puts <<EOS
 input.yml: #{File.expand_path(input)}
 EOS
-    }
+  }
 
-    lopt = case loglevel
-           when Quiet       then '--quiet'
-           when Normal      then '--sys-quiet'
-           when Verbose     then '--app-verbose'
-           when VeryVerbose then '--verbose'
-           end
+  lopt = case loglevel
+         when Quiet       then '--quiet'
+         when Normal      then '--sys-quiet'
+         when Verbose     then '--app-verbose'
+         when VeryVerbose then '--verbose'
+         end
 
-    env = {
-      'EP3_LIBPATH' => ENV['EP3_LIBPATH'],
-      'EP3_PID' => Process.pid.to_s,
-    }
-    if ENV.include? 'DOCKER_HOST'
-      env['DOCKER_HOST'] = ENV['DOCKER_HOST']
-    elsif File.exist?('/var/run/docker.sock')
-      env['DOCKER_HOST'] = 'unix:///var/run/docker.sock'
+  env = {
+    'EP3_LIBPATH' => ENV['EP3_LIBPATH'],
+    'EP3_PID' => Process.pid.to_s,
+  }
+  if ENV.include? 'DOCKER_HOST'
+    env['DOCKER_HOST'] = ENV['DOCKER_HOST']
+  elsif File.exist?('/var/run/docker.sock')
+    env['DOCKER_HOST'] = 'unix:///var/run/docker.sock'
+  end
+
+  IO.popen("medal workdir/root.yml -i workdir/init.yml --workdir=workdir --tmpdir=tmpdir --leave-tmpdir #{lopt} --log=/dev/stdout", 'r+',
+           :chdir=> dir) { |io|
+    open(File.join(dir, 'medal-log.json'), 'w') do |log|
+      io.each_line { |l|
+        warn l
+        log.puts l
+      }
+    ensure
+      rest = io.read
+      warn rest
+      log.puts rest
+      log.flush
     end
-
-    IO.popen("medal workdir/root.yml -i workdir/init.yml --workdir=workdir --tmpdir=tmpdir --leave-tmpdir #{lopt} --log=/dev/stdout", 'r+',
-             :chdir=> dir) { |io|
-      open(File.join(dir, 'medal-log.json'), 'w') do |log|
-        io.each_line { |l|
-          warn l
-          log.puts l
-        }
-      ensure
-        rest = io.read
-        warn rest
-        log.puts rest
-        log.flush
-      end
-    }
-    status = Process.last_status
-    if !status.nil? and status.exited?
-      status.exitstatus
-    else
-      1
-    end
-  rescue Interrupt
-    # nop
-    1
-  rescue SignalException
-    # nop
+  }
+  status = Process.last_status
+  if !status.nil? and status.exited?
+    status.exitstatus
+  else
     1
   end
+rescue Interrupt
+  # nop
+  1
+rescue SignalException
+  # nop
+  1
 end
 
 if $0 == __FILE__
