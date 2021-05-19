0b79556eb0d3ad1262354644ec5f3e3a3af16f2a
Tomoya Tanjo <ttanjo@gmail.com>
2021-05-19 08:01:53 +0000

    [cwl-metrics] Update version to 0.2.0

--- .devcontainer/setup-es.sh
+++ .devcontainer/setup-es.sh
@@ -23,22 +23,6 @@ create_index_workflow() {
   curl -s --header "Content-Type:application/json" -XPUT $ES_HOST:$ES_PORT/workflow -d '{
     "settings": {
       "index.mapping.total_fields.limit": 5000
-    },
-    "mappings": {
-      "properties": {
-        "workflow": {
-          "properties": {
-            "start_date": {
-              "type": "date",
-              "format": "yyyy-MM-dd HH:mm:ss"
-            },
-            "end_date": {
-              "type": "date",
-              "format": "yyyy-MM-dd HH:mm:ss"
-            }
-          }
-        }
-      }
     }
   }'
 }
